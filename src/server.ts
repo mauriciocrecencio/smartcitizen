0a5da2b36124e4999a2cc9163c65ded04c96f315
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-07-19 17:31:26 -0300

    add process env port variable

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 
 app.use("/usuario", usersRoutes);
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running!"));
