317bb1ee5d00390cd4d26128938a5848b3945d57
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-05-11 17:54:02 -0300

    add node version to package.json and .env variables for deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,25 @@
 import { DataSource } from "typeorm";
 import User from "./models/User";
 
+const USERNAME = process.env.POSTGRES_USER || 'docker'
+const PASSWORD = process.env.POSTGRES_PASSWORD || 'docker'
+const HOST = process.env.HOST || 'localhost'
+const PORT = process.env.PORT || '5432'
+const POSTGRES_DB = process.env.POSTGRES_DB || 'auth_users'
+
+
 export const AppDataSource = new DataSource({
+    url:  process.env.URL || `postgres://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/${POSTGRES_DB}`,
+    // url:  `postgres://docker:docker@localhost:5432/auth_users`,
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "docker",
-    password: "docker",
-    database: "auth_users",
-    synchronize: true,
-    logging: true,
-    entities: [User],
-    subscribers: [],
-    migrations: [],
+    // host: process.env.HOST || "localhost",
+    // port: Number(process.env.PORT) || 5432,
+    // username: process.env.POSTGRES_USER || "docker",
+    // password: process.env.POSTGRES_PASSWORD || "docker",
+    // database: process.env.POSTGRES_DB || "auth_users",
+    // synchronize: true,
+    // logging: true,
+    // entities: [User],
+    // subscribers: [],
+    // migrations: [],
 })
