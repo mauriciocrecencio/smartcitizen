4202a9d96a61c11999d80d1509b85b3bb423c443
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-07-21 15:25:36 -0300

    add cors support

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import express from "express";
 import "./database";
 import usersRoutes from "./routes/users.routes";
 
+var cors = require('cors')
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/usuario", usersRoutes);
 
